aadde169bbc872b4e87952b70c00c1daf4cb27f6
Srinivasa Moorty Nalam <nvs.moorty@gmail.com>
2026-08-28 20:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (54.31%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int answer = 0;
+        while (x != 0) {
+            int digit = x % 10;
+
+            if (answer > INT_MAX / 10 || (answer == INT_MAX / 10 && digit > 7))
+            {
+                return 0;
+            }
+            if (answer < INT_MIN / 10 || (answer == INT_MIN / 10 && digit < -8))
+            {
+                return 0;
+            }
+            answer = answer * 10 + digit;
+            x /= 10;
+        }
+        
+            return answer;
+           
+    }
+};
